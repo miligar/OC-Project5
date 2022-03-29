e1ec78aa0ea776ce64d8d3f901b9f16a2315de77
miligar <emilguiga@gmail.com>
2022-03-29 16:49:53 +0100

    product page done

---
+++ front/js/product.js
@@ -0,0 +1,83 @@
+const params = new URLSearchParams(window.location.search);
+const idProduct = params.get('id');
+console.log(idProduct);
+
+
+
+fetch('http://localhost:3000/api/products/' + idProduct)
+.then(response => {return response.json();
+})
+.then(data => {
+   console.log(data);
+   productDisplay(data);
+})
+.catch((error) => {console.log("Error from API");})
+
+function productDisplay(product){
+
+    const imgBox = document.querySelector('.item__img');
+    const itemImg = document.createElement('img');
+    itemImg.setAttribute('src', product.imageUrl);
+    itemImg.alt = product.altTxt;
+    imgBox.appendChild(itemImg);
+
+    const priceBox = document.getElementById('price');
+    const itemPrice = product.price;
+    priceBox.textContent = itemPrice;
+
+    const descriptionBox = document.getElementById('description');
+    const itemDescription = product.description;
+    descriptionBox.textContent = itemDescription;
+
+    const colorBox = document.getElementById('colors');
+
+    for (let color of product.colors) {
+        const colorOption = document.createElement('option');
+        colorOption.textContent = color;
+        colorBox.appendChild(colorOption);
+    }
+    cartBuild();
+}
+
+//the cart can be made as an array containing three things: - the product ID, - the quantity , -color
+//use LocalStorage to access this array from the product page
+//When adding the same product to the cart (same ID and colour), increase the quantity of product in the array
+function cartBuild () {
+
+    const addButton = document.getElementById('addToCart');
+    const productQuantity = document.getElementById('quantity');
+    const color = document.getElementById('colors');
+
+    let cartArray = [];
+
+    addButton.addEventListener('click', ($event) => {
+
+        if(localStorage.getItem('cartArray')!==null){
+          cartArray = JSON.parse(localStorage.getItem('cartArray'));
+        }
+
+        let newItem = {};
+        if (productQuantity.value>=1 && productQuantity.value <=100 && color.value){
+            newItem = {
+                idProduct: idProduct,
+                quantity: parseInt(productQuantity.value),
+                color: color.value
+            };
+            let idToLook = newItem.idProduct;
+            let findItem = cartArray.find((idProduct, idToLook) => {
+                if (idProduct.color === newItem.color) {
+                    idProduct.quantity =  idProduct.quantity + newItem.quantity;
+                    return true;
+                } 
+            });
+            if (!findItem){
+                cartArray.push(newItem);
+            }
+            productQuantity.value=0;
+            localStorage.setItem('cartArray', JSON.stringify(cartArray));
+
+            console.log(cartArray);
+            console.log(localStorage.getItem('cartArray'));
+        };
+    });
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,24 +1,30 @@
-
-
+//product data will be requested from backend
+//once that data is received, a function 'createCards' will retrieve the info we need and build the cards
 fetch('http://localhost:3000/api/products/')
-    .then(response => response.json() )
+    .then(response => {return response.json();
+     })
     .then(data => {
         console.log(data);
         createCards(data);
     })
-    .catch(error => console.log(error))
+    .catch((error) => {
+        console.log("Error from API");
+    })
 
 
-
+//Using data the function 'createCards' will access through a loop to all necessary product details to assemble the cards
 function createCards(products) {
 
     const cardSection = document.getElementById('items');
 
     for(let i = 0; i < products.length; i++) {
+
+        //'item' will have the individual product's info to build the card: image, title and description
         let item = document.createElement('article');
+
         const itemImg = document.createElement('img');
         itemImg.setAttribute('src', products[i].imageUrl);
-        itemImg.atl = products[i].altTxt;
+        itemImg.alt = products[i].altTxt;
 
         const itemName = document.createElement('h3');
         itemName.classList.add('productName');
@@ -28,13 +34,14 @@ function createCards(products) {
         itemDescription.classList.add('productDescription');
         itemDescription.textContent = products[i].description;
 
+        //Once all info needed has been assembled, it will be appended in order to create the content of the card
         item.appendChild(itemImg);        
         item.appendChild(itemName);
         item.appendChild(itemDescription);
 
+        //Item will be appended to a parent link and the whole will be the card, and then cards appended to the section: <section class="items" id="items">
         const itemLink = document.createElement('a');
         itemLink.setAttribute('href', `product.html?id=${products[i]._id}`);
-
         itemLink.appendChild(item);
         cardSection.appendChild(itemLink);                
     }
